5baf8c9658ae47ff2cdaca59ab1afb72b679599e
Dayanna Montaño <dayannastefania88@gmail.com>
2026-03-17 16:48:58 -0500

    paginas acts

--- ejercicio.py
+++
@@ -1,8 +0,0 @@
-#ingresar un numero en este caso la edad del usuario
-edad =int(input("Ingrese su edad:"))
-#verificar si la edad ingresada es igual o mayor a 18 
-if edad >=18:
-    #si es mayor o igual a 18
-    print("El usuario puede votar")
-else:
-    print("El usuario no puede votar")
